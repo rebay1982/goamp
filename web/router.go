8e4f00853ebe83f165fe5c3bfe60abff99c6ef49
Reda Baydoun <reda.baydoun@gmail.com>
2019-05-11 09:21:17 -0400

    [FT] Addded basic REST endpoints, fiddled with bogus artist info

--- web/router.go
+++ web/router.go
@@ -6,11 +6,29 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type artistInfo struct {
+	Artist string
+	Albums []albumInfo
+}
+
+type albumInfo struct {
+	Album  string
+	Tracks []trackInfo
+}
+
+type trackInfo struct {
+	Track       string
+	TrackNumber int
+	Length      int
+}
+
 var r = gin.Default()
 
 // SetupRouter This method sets up the REST method router.
 func SetupRouter() {
 	r.GET("/ping", ping)
+	r.GET("/artists", listArtists)
+	r.GET("/artists/:artist", listArtistInformation)
 }
 
 // StartWebEngine Starts the Gin web engine
@@ -20,4 +38,47 @@ func StartWebEngine() {
 
 func ping(c *gin.Context) {
 	c.String(http.StatusOK, "pong\n")
+}
+
+func listArtists(c *gin.Context) {
+	artists := getArtistList()
+	var artistNames []string
+
+	// We're only interested in the artist names for now
+	for _, ai := range artists {
+		artistNames = append(artistNames, ai.Artist)
+	}
+
+	c.JSON(http.StatusOK, artistNames)
+}
+
+func listArtistInformation(c *gin.Context) {
+	artistRequested := c.Param("artist")
+	artists := getArtistList()
+
+	for _, ai := range artists {
+		if artistRequested == ai.Artist {
+			c.JSON(http.StatusOK, ai)
+			return
+		}
+	}
+
+	c.String(http.StatusNotFound, "Artist [%s] is not available.", artistRequested)
+}
+
+func getArtistList() []artistInfo {
+	a := []artistInfo{
+		artistInfo{"Judas Priest", []albumInfo{
+			albumInfo{"Painkiller", []trackInfo{
+				trackInfo{"Painkiller", 1, 600},
+				trackInfo{"Hell Patrol", 2, 660}}}}},
+		artistInfo{"Morbid Angel", []albumInfo{
+			albumInfo{"Covenant", []trackInfo{
+				trackInfo{"Rapture", 1, 600},
+				trackInfo{"God of Emptiness", 10, 660}}},
+			albumInfo{"Domination", []trackInfo{
+				trackInfo{"Where the Slime Lives", 2, 660},
+				trackInfo{"Dawn of the Angry", 6, 600}}}}}}
+
+	return a
 }
